c8500a08037caa1e51ee3d9886cd318f853f7bb5
sareasong <sareasong@gmail.com>
2018-01-24 20:59:53 +0900

    0124
    ㅁㅁ

---
+++ js/common.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+  jQuery('.toggle-nav').click(function(e) {
+    jQuery(this).toggleClass('active');
+    jQuery('.menu ul').toggleClass('active');
+    });
+});
